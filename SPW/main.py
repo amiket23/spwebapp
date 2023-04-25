5556f2b17cd51e95aaea1cf100dbb43d93392e38
Amiket Kumar Srivastava <65346396+amiket23@users.noreply.github.com>
2023-04-25 23:41:20 +0100

    update

--- SPW/main.py
+++ SPW/main.py
@@ -29,6 +29,14 @@ class Users(UserMixin, db.Model):
     accesslevel = db.Column(db.String(5), default="user", nullable=False)
     isactive = db.Column(db.String(3), default="yes", nullable=False)
 
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    brand = db.Column(db.String(255), nullable=False)
+    code = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+
 db.init_app(app)
 
 
@@ -70,11 +78,16 @@ def logout():
 
 
 @app.route("/")
-def home():
+def default_path():
     return render_template("index.html")
 
 @app.route("/index")
 def index():
+    return render_template("index.html")
+
+
+@app.route("/home")
+def home():
     return render_template("index.html")
 
 
@@ -83,15 +96,133 @@ def about_us():
     return render_template("about.html")
 
 
-@app.route("/shop")
-@login_required
-def shop():
-    return render_template("shop.html")
-
-
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add_product_to_cart():
+    try:
+        _quantity = int(request.form['quantity'])
+        _code = request.form['code']
+        # validate the received values
+        if _quantity and _code and request.method == 'POST':
+            product_list = Products.query.all()
+            for product in product_list:
+                if product.code == _code:
+                    product_value = product
+                    break
+            product =product_value
+            itemArray = {
+                product.code: {'name': product.name, 'code': product.code, 'quantity': _quantity, 'price': product.price,
+                              'image': product.image, 'total_price': _quantity * product.price}}
+
+            all_total_price = 0
+            all_total_quantity = 0
+            session.modified = True
+            if 'cart_item' in session:
+                if product.code in session['cart_item']:
+                    for key, value in session['cart_item'].items():
+                        if product.code == key:
+                            # session.modified = True
+                            # if session['cart_item'][key]['quantity'] is not None:
+                            #	session['cart_item'][key]['quantity'] = 0
+                            old_quantity = session['cart_item'][key]['quantity']
+                            total_quantity = old_quantity + _quantity
+                            session['cart_item'][key]['quantity'] = total_quantity
+                            session['cart_item'][key]['total_price'] = total_quantity * product.price
+                else:
+                    session['cart_item'] = array_merge(session['cart_item'], itemArray)
+
+                for key, value in session['cart_item'].items():
+                    individual_quantity = int(session['cart_item'][key]['quantity'])
+                    individual_price = float(session['cart_item'][key]['total_price'])
+                    all_total_quantity = all_total_quantity + individual_quantity
+                    all_total_price = all_total_price + individual_price
+            else:
+                session['cart_item'] = itemArray
+                all_total_quantity = all_total_quantity + _quantity
+                all_total_price = all_total_price + _quantity * product.price
+
+            session['all_total_quantity'] = all_total_quantity
+            session['all_total_price'] = all_total_price
+            return redirect(url_for('shop'))
+        else:
+            return 'Error while adding item to cart'
+    except Exception as e:
+        print(e)
+
+
+
+@app.route('/shop')
+@login_required
+def shop():
+    try:
+        product = Products.query.all()
+        return render_template('shop.html', products=product)
+    except Exception as e:
+        print(e)
+
+
+@app.route('/empty')
+@login_required
+def empty_cart():
+    try:
+        session.clear()
+        return redirect(url_for('shop'))
+    except Exception as e:
+        print(e)
+
+
+@app.route('/delete/<string:code>')
+@login_required
+def delete_product(code):
+    try:
+        all_total_price = 0
+        all_total_quantity = 0
+        session.modified = True
+
+        for item in session['cart_item'].items():
+            if item[0] == code:
+                session['cart_item'].pop(item[0], None)
+                if 'cart_item' in session:
+                    for key, value in session['cart_item'].items():
+                        individual_quantity = int(session['cart_item'][key]['quantity'])
+                        individual_price = float(session['cart_item'][key]['total_price'])
+                        all_total_quantity = all_total_quantity + individual_quantity
+                        all_total_price = all_total_price + individual_price
+                break
+
+        if all_total_quantity == 0:
+            session.clear()
+        else:
+            session['all_total_quantity'] = all_total_quantity
+            session['all_total_price'] = all_total_price
+
+        # return redirect('/')
+        return redirect(url_for('shop'))
+    except Exception as e:
+        print(e)
+
+
+@app.route('/cart')
+@login_required
+def cart_load():
+    if request.method == "POST":
+        # execute db query to store form info with username date and order status/id
+    return render_template("cart.html")
+
+
+def array_merge(first_array, second_array):
+    if isinstance(first_array, list) and isinstance(second_array, list):
+        return first_array + second_array
+    elif isinstance(first_array, dict) and isinstance(second_array, dict):
+        return dict(list(first_array.items()) + list(second_array.items()))
+    elif isinstance(first_array, set) and isinstance(second_array, set):
+        return first_array.union(second_array)
+    return False
 
 
 if __name__ == "__main__":
